f5d2e907487082935181209f8c771842d65a2cfb
Sebastian Martino <SBFMartino@gmail.com>
2021-10-10 14:07:05 -0400

    Add files via upload

---
+++ PiCamClient.c
@@ -0,0 +1,46 @@
+/**
+* @author SebastianMartino
+* Test client side application to connect to Arducam_demo
+*
+*/
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define PORT 2002
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[512];
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "172.18.128.93", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    printf("Connection Made\n");
+    valread = read(sock , buffer, 512);
+    fprintf(stdout, "%s\n",buffer);
+    return 0;
+}
