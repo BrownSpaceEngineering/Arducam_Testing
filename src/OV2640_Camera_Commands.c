9b9fed652738b4261272f7f76667017621d66134
jrr6 <7482866+jrr6@users.noreply.github.com>
2023-10-29 13:25:58 -0400

    Start working on SCCB

--- src/OV2640_Camera_Commands.c
+++ src/OV2640_Camera_Commands.c
@@ -68,16 +68,16 @@ void OV2640_set_resolution(uint8_t resolution) {
     // TODO: check these calls
     switch (resolution) {
         case OV2640_160x120:
-            wrSensorRegs8_8(OV2640_160x120_JPEG);
+            OV2640_sccb_write_8bit_reg_array(OV2640_160x120_JPEG);
             break;
         case OV2640_320x240:
-            wrSensorRegs8_8(OV2640_320x240_JPEG);
+            OV2640_sccb_write_8bit_reg_array(OV2640_320x240_JPEG);
             break;
         case OV2640_640x480:
-            wrSensorRegs8_8(OV2640_640x480_JPEG);
+            OV2640_sccb_write_8bit_reg_array(OV2640_640x480_JPEG);
             break;
         case OV2640_1024x768:
-            wrSensorRegs8_8(OV2640_1024x768_JPEG);
+            OV2640_sccb_write_8bit_reg_array(OV2640_1024x768_JPEG);
             break;
         default:
             // TODO: some sort of error?
@@ -117,9 +117,9 @@ uint8_t OV2640_module_detect(void) {
     uint8_t attempts = 0;
     while (attempts < OV2640_DETECTION_ATTEMPTS) {
         // TODO: check these calls
-        OV2640_i2c_sccb_reg_write_8_8(0xff, 0x01);
-        rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
-        rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+        OV2640_sccb_write_8bit_reg(0xff, 0x01);
+        OV2640_sccb_read_8bit_reg(OV2640_CHIPID_HIGH, &vid);
+        OV2640_sccb_read_8bit_reg(OV2640_CHIPID_LOW, &pid);
         // TODO: this is straight from the OV2640 sample code, but clearly
         // something is awry here because ((pid != 0x41) || ( pid != 0x42)) is
         // tautological
@@ -221,40 +221,4 @@ uint8_t OV2640_spi_transceive(uint8_t send) {
         // Somewhere account for different status codes (e.g., ERR_BUSY == -4)
         return 0x00;
     }
-}
-
-// TODO: implement!
-void OV2640_i2c_sccb_reg_multiwrite_8_8();
-
-// TODO: return a status code (check the io_writes & i2c_* calls!)
-/**
- * Writes a sensor register with an 8-bit address over I2C.
- * @param reg_id the ID of the register to write.
- * @param reg_data the data to write to the register.
- */
-void OV2640_i2c_sccb_reg_write_8_8(uint8_t reg_id, uint8_t *reg_data) {
-    struct io_descriptor io;
-    i2c_m_sync_get_io_descriptor(&I2C_0, &io);
-    i2c_m_sync_enable(&I2C_0);
-    i2c_m_sync_set_slaveaddr(&I2C_0, OV2640_I2C_ADDR, I2C_M_SEVEN);
-    io_write(&io, reg_id, 1);
-    io_write(&io, reg_data, 1);
-}
-
-// TODO: return a status code (check the io_writes & i2c_* calls!)
-/**
- * Writes a sensor register with a 16-bit address over I2C.
- * @param reg_id the ID of the register to write.
- * @param reg_data the data to write to the register.
- */
-void OV2640_i2c_sccb_reg_write_16_8(uint16_t reg_id, uint8_t *reg_data) {
-    struct io_descriptor io;
-    i2c_m_sync_get_io_descriptor(&I2C_0, &io);
-    i2c_m_sync_enable(&I2C_0);
-    i2c_m_sync_set_slaveaddr(&I2C_0, OV2640_I2C_ADDR, I2C_M_SEVEN);
-    // TODO: check this is okay (using length = 2)
-    // Do we want to be using i2c_m_sync_cmd_[read/write]?
-    // May be an issue for this one b/c we need *two* sensor addr bytes
-    io_write(&io, reg_id, 2);
-    io_write(&io, reg_data, 1);
 }
--- src/OV2640_sccb.c
+++ src/OV2640_sccb.c
@@ -1,11 +1,77 @@
 /*
- * OV2640_sccb_bus.c
+ * OV2640_sccb.c
  * Created: 12/30/21
  * Author: Brown Space Engineering
  */
 #include "OV2640_sccb.h"
 #include "OV2640_regs.h"
 
-uint8_t OV2640_sccb_write(uint8_t byte) {
 
+// TODO: return a status code (check the io_write/read & i2c_* calls!)
+/**
+ * Reads a sensor register with an 8-bit address over I2C.
+ * @param reg_id the ID of the register to write.
+ * @param reg_data_buf a buffer in which the register contents will be loaded by the function.
+ */
+void OV2640_sccb_read_8bit_reg(uint8_t reg_id, unsigned char *reg_data_buf) {
+    // Mask the register ID to specify a read
+    uint8_t reg_id_masked = reg_id | 0x01;
+    struct io_descriptor io;
+    i2c_m_sync_get_io_descriptor(&I2C_0, &io);
+    i2c_m_sync_enable(&I2C_0);
+    i2c_m_sync_set_slaveaddr(&I2C_0, OV2640_I2C_ADDR, I2C_M_SEVEN);
+    io_write(&io, &reg_id_masked, 1);
+    io_read(&io, reg_data_buf, 1);
+}
+
+// TODO: return a status code (check the io_write/read & i2c_* calls!)
+/**
+ * Reads a sensor register with an 8-bit address over I2C.
+ * @param reg_id the ID of the register to write.
+ * @param reg_data_buf a buffer in which the register contents will be loaded by the function.
+ */
+ // FIXME: `rdSensorReg16_8` writes 0x78 instead of the sensor address at the start and then writes 0x79 at the end --
+ // WHY? and how do we do the equivalent thing using the I2C stuff here?
+void OV2640_sccb_read_16bit_reg(uint16_t reg_id, unsigned char *reg_data_buf) {
+    // Mask the register ID to specify a read
+    uint16_t reg_id_masked = reg_id | 0x01;
+    struct io_descriptor io;
+    i2c_m_sync_get_io_descriptor(&I2C_0, &io);
+    i2c_m_sync_enable(&I2C_0);
+    i2c_m_sync_set_slaveaddr(&I2C_0, OV2640_I2C_ADDR, I2C_M_SEVEN);
+    io_write(&io, &reg_id_masked, 1);
+    io_read(&io, reg_data_buf, 1);
+}
+
+// TODO: return a status code (check the io_writes & i2c_* calls!)
+/**
+ * Writes a byte to a sensor register with an 8-bit address over I2C.
+ * @param reg_id the ID of the register to write.
+ * @param reg_data the data (byte) to write to the register.
+ */
+void OV2640_sccb_write_8bit_reg(uint8_t reg_id, uint8_t reg_data) {
+    struct io_descriptor io;
+    i2c_m_sync_get_io_descriptor(&I2C_0, &io);
+    i2c_m_sync_enable(&I2C_0);
+    i2c_m_sync_set_slaveaddr(&I2C_0, OV2640_I2C_ADDR, I2C_M_SEVEN);
+    io_write(&io, &reg_id, 1);
+    io_write(&io, &reg_data, 1);
+}
+
+// TODO: return a status code (check the io_writes & i2c_* calls!)
+/**
+ * Writes a byte to a sensor register with a 16-bit address over I2C.
+ * @param reg_id the ID of the register to write.
+ * @param reg_data the data (byte) to write to the register.
+ */
+void OV2640_sccb_write_16bit_reg(uint16_t reg_id, uint8_t reg_data) {
+    struct io_descriptor io;
+    i2c_m_sync_get_io_descriptor(&I2C_0, &io);
+    i2c_m_sync_enable(&I2C_0);
+    i2c_m_sync_set_slaveaddr(&I2C_0, OV2640_I2C_ADDR, I2C_M_SEVEN);
+    // TODO: check this is okay (using length = 2)
+    // Do we want to be using i2c_m_sync_cmd_[read/write]?
+    // May be an issue for this one b/c we need *two* sensor addr bytes
+    io_write(&io, &reg_id, 2);
+    io_write(&io, &reg_data, 1);
 }
